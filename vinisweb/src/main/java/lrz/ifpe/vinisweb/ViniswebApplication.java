40e99e1604b10ec73a3c9b85374f5fc4461de208
luanyrose <lrlm@discente.ifpe.edu.br>
2025-11-25 10:36:24 -0300

    spring buto

---
+++ vinisweb/src/main/java/lrz/ifpe/vinisweb/ViniswebApplication.java
@@ -0,0 +1,13 @@
+package lrz.ifpe.vinisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViniswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViniswebApplication.class, args);
+	}
+
+}
---
+++ vinisweb/src/test/java/lrz/ifpe/vinisweb/ViniswebApplicationTests.java
@@ -0,0 +1,13 @@
+package lrz.ifpe.vinisweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViniswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
